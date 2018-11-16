332b7409d29e3cca5f0239442d0674e5b989803f
Nithin Bekal <nithinbekal@gmail.com>
2018-11-15 23:37:38 -0500

    Run mix format on mix.exs

--- mix.exs
+++ mix.exs
@@ -2,17 +2,18 @@ defmodule AmazonBooks.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :amazon_books,
-     version: "0.3.0",
-     elixir: "~> 1.7",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     docs: docs(),
-     package: package(),
-     description: """
-     Fetch book information from Amazon API"
-     """
+    [
+      app: :amazon_books,
+      version: "0.3.0",
+      elixir: "~> 1.7",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: """
+      Fetch book information from Amazon API"
+      """
     ]
   end
 
@@ -25,17 +26,17 @@ defmodule AmazonBooks.Mixfile do
       {:aws_sign_url, "~> 0.1.0"},
       {:httpoison, "~> 1.4.0"},
       {:sweet_xml, "~> 0.6"},
-
-      {:ex_doc, ">= 0.19.0", only: :dev},
+      {:ex_doc, ">= 0.19.0", only: :dev}
     ]
   end
 
   def package do
-    [ name: :amazon_books,
+    [
+      name: :amazon_books,
       files: ["lib", "mix.exs"],
       maintainers: ["Nithin Bekal"],
       licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/nithinbekal/amazon_books"},
+      links: %{"Github" => "https://github.com/nithinbekal/amazon_books"}
     ]
   end
 
