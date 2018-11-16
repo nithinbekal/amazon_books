95a11ee2c00ada26884a84329b84120d03876ff9
Nithin Bekal <nithinbekal@gmail.com>
2018-11-15 23:36:58 -0500

    Set elixir version to 1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AmazonBooks.Mixfile do
   def project do
     [app: :amazon_books,
      version: "0.3.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
